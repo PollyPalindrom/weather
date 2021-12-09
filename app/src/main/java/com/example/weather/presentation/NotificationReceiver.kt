e1f8b6e662c6be62c2e535187031a363ba1873d3
PollyPalindrom <polinaahrameiko@gmail.com>
2021-12-09 21:54:55 +0300

    refactor

--- app/src/main/java/com/example/weather/presentation/NotificationReceiver.kt
+++ app/src/main/java/com/example/weather/presentation/NotificationReceiver.kt
@@ -8,73 +8,69 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
-import androidx.room.Room
 import com.example.weather.R
 import com.example.weather.common.ConnectionManager
 import com.example.weather.common.CurrentLocationManager
 import com.example.weather.data.database.AppDatabase
-import com.example.weather.data.database.WeatherDao
 import com.example.weather.data.remote.BoredApi
 import com.example.weather.data.remote.WeatherApi
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Inject
 import kotlin.random.Random
-
-private const val DATABASE_NAME = "LastWeatherInfo"
 
 @AndroidEntryPoint
 class NotificationReceiver :
     BroadcastReceiver() {
+    @Inject
+    lateinit var database: AppDatabase
 
-    private lateinit var weatherDao: WeatherDao
+    @Inject
+    lateinit var weatherApi: WeatherApi
 
-    private val weatherApi = Retrofit.Builder()
-        .baseUrl("https://api.openweathermap.org/data/2.5/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build().create(WeatherApi::class.java)
-    private val boredApi = Retrofit.Builder()
-        .baseUrl("https://www.boredapi.com/api/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build().create(BoredApi::class.java)
+    @Inject
+    lateinit var boredApi: BoredApi
 
+    @Inject
     lateinit var currentLocationManager: CurrentLocationManager
 
+    @Inject
     lateinit var connectionManager: ConnectionManager
 
-    private lateinit var context: Context
-
     override fun onReceive(p0: Context?, p1: Intent?) {
-        if (p0 != null) {
-            context = p0
-        }
-        weatherDao = Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME)
-            .build().weatherDao()
-        currentLocationManager = p0?.let { CurrentLocationManager(it) }!!
-        connectionManager = ConnectionManager(p0)
         if (connectionManager.checkForInternet()) {
-            getCurrentWeatherHere()
+            if (p0 != null) {
+                getCurrentWeatherHere(p0)
+            }
         } else {
-            createNotification("Have a nice day!")
-        }
-    }
-
-    private fun getCurrentWeatherHere() {
-        CoroutineScope(Dispatchers.IO).launch {
-            if (connectionManager.checkForInternet()) {
-                val weather = weatherDao.getAllList()
-                val lastLocationWeather =
-                    weatherApi.getCurrentWeather(weather[0].lat, weather[0].lon)
-                val activity = boredApi.getActivity()
-                createNotification("Temperature:" + lastLocationWeather.main.temp + "°" + "\nMaybe it is a good day to:" + activity.activity)
+            if (p0 != null) {
+                createNotification("Have a nice day!", p0)
             }
         }
     }
 
-    private fun createNotification(text: String) {
+    private fun getCurrentWeatherHere(context: Context) {
+        CoroutineScope(Dispatchers.IO).launch {
+            if (connectionManager.checkForInternet()) {
+                val weather = database.weatherDao().getAllList()
+                val lastLocationWeather =
+                    if (weather.isNotEmpty()) weatherApi.getCurrentWeather(
+                        weather[0].lat,
+                        weather[0].lon
+                    )
+                    else null
+                val activity = boredApi.getActivity()
+                createNotification(
+                    "Temperature:" + lastLocationWeather?.main?.temp + "°" + "\nMaybe it is a good day to:" + activity.activity,
+                    context
+                )
+            }
+        }
+    }
+
+    private fun createNotification(text: String, context: Context) {
         val intent = Intent(context, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         val pendingIntent = PendingIntent.getActivity(
